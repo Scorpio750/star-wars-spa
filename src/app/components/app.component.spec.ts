95f380570ca4b733bfca254551750cdf64facd18
Patrick Wu <scorpio7500@gmail.com>
2017-11-02 23:13:22 -0400

    add spec for app.component

---
+++ src/app/components/app.component.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { CharacterComponent } from './character/character.component';
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        CharacterComponent
+      ],
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+});
