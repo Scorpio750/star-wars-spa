20dd5e56cdd49f3747209de75c2f46bfbc6fc9c9
Patrick Wu <scorpio7500@gmail.com>
2017-11-02 23:21:01 -0400

    fix misnamed config var

--- karma.conf.js
+++ karma.conf.js
@@ -41,7 +41,7 @@ module.exports = function (config) {
 	};
 
 	if(process.env.TRAVIS){
-		configuration.browsers = ['Chrome_travis_ci'];
+		cfg.browsers = ['Chrome_travis_ci'];
 	};
 
 	config.set(cfg);
